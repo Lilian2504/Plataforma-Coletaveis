6efa2af7f3a5ef1765a508dc2597a3c2c4885cd5
Lilian2504 <61809430+Lilian2504@users.noreply.github.com>
2020-03-05 22:02:43 -0300

    Add files via upload

---
+++ AtivarParticulasquandoColide.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtivarParticulasquandoColide : MonoBehaviour
+{
+	public ParticleSystem ps;
+    // Start is called before the first frame update
+    void Start()
+    {
+		ps.Stop();
+    }
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.name == "Felpudo_Seq_Idle0001"){
+			ps.Play();
+		}
+	}
+    // Update is called once per frame
+   
+}
---
+++ MoveFofura.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveFofura : MonoBehaviour
+{
+	public float velocidadeF;
+	public float forcaPulo;
+	private Rigidbody2D rb;
+	private int count;
+	public GameObject Parede;
+	
+	
+	// Start is called before the first frame update
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		count=0;
+		
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		if(Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(Vector2.left*-velocidadeF * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(Vector2.left*velocidadeF * Time.deltaTime);
+		}  
+		if (Input.GetKeyDown(KeyCode.W)) {
+			rb.AddForce(transform.up * forcaPulo);
+		}
+		
+	}
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag=="chave"){
+			other.gameObject.SetActive(false);
+			count=count+1;
+			if (count >= 15){
+				Parede.SetActive(false);
+			}
+			
+		}
+
+	}
+}
